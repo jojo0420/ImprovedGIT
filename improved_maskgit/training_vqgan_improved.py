74f39ac07cd5a59850e7ebe16631b68d198a8d9d
ztj0420 <ztj0420@gmail.com>
2022-05-02 22:44:20 -0400

    to train flower

--- improved_maskgit/training_vqgan_improved.py
+++ improved_maskgit/training_vqgan_improved.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     parser.add_argument('--image-channels', type=int, default=3, help='Number of channels of images (default: 3)')
     parser.add_argument('--dataset-path', type=str, default='/data', help='Path to data (default: /data)')
     parser.add_argument('--device', type=str, default="cuda", help='Which device the training is on')
-    parser.add_argument('--batch-size', type=int, default=6, help='Input batch size for training (default: 6)')
+    parser.add_argument('--batch-size', type=int, default=4, help='Input batch size for training (default: 6)')
     parser.add_argument('--epochs', type=int, default=50, help='Number of epochs to train (default: 50)')
     parser.add_argument('--learning-rate', type=float, default=2.25e-05, help='Learning rate (default: 2.25e-05)')
     parser.add_argument('--beta1', type=float, default=0.5, help='Adam beta param (default: 0.5)')
